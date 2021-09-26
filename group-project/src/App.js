9d82e4a4071313aa3e804933186223ec043ddf07
minh son do <minh-son.do@students.mq.edu.au>
2021-09-26 17:29:11 +1000

    Added a login form

--- group-project/src/App.js
+++ group-project/src/App.js
@@ -1,9 +1,21 @@
+import React, {useState, useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import LoginForm from './LoginForm'
 
 function App() {
+
+  const [user, setUser] = useState(null)
   return (
     <div className="App">
+
+    <div className="row">
+      <div className="u-pull-right">
+        <LoginForm user={user} setUser={setUser}/>
+      </div>
+    </div>
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
