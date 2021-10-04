bb98d70c0ec8a3a876785aaddf11c57bb5d5daa7
SandraTrinh <sandra.trinh@students.mq.edu.au>
2021-10-04 14:45:35 +1100

    remove default react app logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react'
-import logo from './logo.svg';
 import './App.css';
 import LoginForm from './LoginForm'
 import productService from './services/products'
@@ -62,30 +61,8 @@ function App() {
             </Route> */}
         </Switch>
 
-
-
-
-
-
-
       </Router>
    
-
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
