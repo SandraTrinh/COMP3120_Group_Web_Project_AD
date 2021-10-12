a1fa72cf3ab5eeb3d0270f2fac9ac5a76b6ebf64
Chowdary1999 <51407838+Chowdary1999@users.noreply.github.com>
2021-10-13 10:20:10 +1100

    Added Tests for MongoDB Connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,33 @@
 require('dotenv').config()
-const app = require('./app')
+const express = require('express') 
+const cors = require('cors')
+const apiRouter = require("./controllers/api")
+const middleware = require("./utils/middleware")
+var session = require('express-session')
+
+
+const app = express() 
+
+app.use(session({
+  secret: 'comp3120Assignment',
+  resave: false,
+  saveUninitialized: true,
+  httpOnly: true,
+  cookie: {
+      path: '/api', 
+      sameSite: 'strict'
+  }
+}))
+app.use(cors())
+app.use(express.json())
+
+app.use(middleware.requestLogger)
+app.use(express.static('build'))
+app.use(apiRouter)
+app.use(middleware.errorMiddleware)
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
