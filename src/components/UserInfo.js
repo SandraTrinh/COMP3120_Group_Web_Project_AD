7ea18720cdfc71a2d8b5075b7d8db7dec9f2ea18
Chowdary1999 <51407838+Chowdary1999@users.noreply.github.com>
2021-10-15 18:26:41 +1100

    Added Database information for Feedback

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,58 +1,53 @@
-import React, {useState, useEffect} from 'react'
-
-import {Link} from 'react-router-dom'
-
+import React, { useState, useEffect } from 'react'
+import Pdf from "react-to-pdf";
+import { Link } from 'react-router-dom'
 import UserService from '../services/UserData.js'
 
-const UserInfo = ({user}) => {
-    const [userData, setMyData] = useState(null)
+const ref = React.createRef();
 
+const UserInfo = ({ user }) => {
+    const [userData, setMyData] = useState(null)
     useEffect(() => {
-        UserService.getUserData({user})
-        .then(data => {
-            setMyData(data)
-            //console.log("data is", data)
-        })
-        .catch(error => {
-            console.log("Error:", error.response.data)
-        })
+        UserService.getUserData({ user })
+            .then(data => {
+                setMyData(data)
+                //console.log("data is", data)
+            })
+            .catch(error => {
+                console.log("Error:", error.response.data)
+            })
     }, [])
 
-    if(user!=null) {
-        if(userData !=null) {
-            return(
+    if (user != null) {
+        if (userData != null) {
+            return (
                 <div className="vaccination col-12">
-        
-                    <ul className="vaccination-list">
-                    <li> Full Name: {userData.name}     </li>
-                    <li> Territory Name: {userData.territoryName}     </li>
-                    <li> Vaccine name: {userData.vaccineName}     </li>
-                    <li> Vaccination status: {userData.status}     </li>
-                    <li> First Dose Date: {userData.FirstDose}     </li>
-                    <li> Second Dose Date: {userData.SecondDose}     </li>
-        
-                    
-                    </ul>
-        
-                </div> 
-                )
+                    <div className="App">
+                        <Pdf targetRef={ref} filename="Vaccine-Cert.pdf">
+                            {({ toPdf }) => <button onClick={toPdf}>Download Your Digital Certificate</button>}
+                        </Pdf>
+                        
+                    </div>
+                </div>
+
+            )
         } else {
-            return(
-                <div> 
-                <p> Loading ...</p>
+            return (
+                <div>
+                    <p> Loading ...</p>
                 </div>
             )
         }
-       
+
     } else {
-       
-        return(
-            <div> 
-                 <Link className="App-link" to="/login">Click me to log in</Link>
+
+        return (
+            <div>
+                <Link className="App-link" to="/login">Click me to log in</Link>
             </div>
         )
 
     }
-
 }
+
 export default UserInfo
