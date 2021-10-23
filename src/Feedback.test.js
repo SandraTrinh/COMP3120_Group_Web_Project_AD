301733f57c6c3d653c25073ba6ed6ba49f7f1c3e
jackchair <68882404+jackchair@users.noreply.github.com>
2021-10-23 16:06:43 +0800

    Added test for feedback function

---
+++ src/Feedback.test.js
@@ -0,0 +1,35 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { fireEvent, render } from '@testing-library/react'
+import fs from 'fs'
+import Feedback from './components/Feedback'
+
+/**
+ * Read sample data for testing
+ * 
+ * @param {String} fileName JSON data filename
+ * @returns {Array} an array of like records
+ */
+const sampleData =  (fileName) => {
+    const rawData = fs.readFileSync(fileName)
+    const data = JSON.parse(rawData)
+
+    return data.userfeedback
+}
+
+describe('Feedback component', () => {
+
+    test( 'Get feedback function work', () => {
+        const contents = sampleData('server/feedback.json')
+        const Feedback = jest.fn()
+
+        const component = render(
+            <Feedback contents={contents} Feedback={Feedback}/>
+        )
+
+        contents.map(c => expect(component.container).toHaveTextContent(c.content))
+    })
+})
