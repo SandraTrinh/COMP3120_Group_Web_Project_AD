3731ab8e266ca3390f24babf6ea9c6f3b99cf7ae
SandraTrinh <sandra.trinh@students.mq.edu.au>
2021-10-26 02:09:18 +1100

    booking CSS and some form input changes

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -39,99 +39,172 @@ const Bookings = () => {
       });
       
     return (
+      //<div className="home-page col-12">
         <div className="bookingform" >
+          <div className="title">
+            <h3>Book your COVID-19 Vaccination</h3>
+          </div>
           <div className="question">
+            <p>Please Select a Booking Date:</p>
             <DatePicker selected={myDate} onChange={(date) => setMyDate(date)} />
           </div>
          <div className="question">
+           <p>Select the vaccine:</p>
             <Select  styles={customStyles}  options={choices}/>
          </div>
                   
           <form onSubmit={handleSubmit(onSubmit)}>
-              
-              <div className="question"> 
-                <input type="checkbox" />
-                <label htmlFor="1"> Have you had an allergic reaction to a previous dose of a COVID-19 vaccine?</label>
-                <p> </p>
-              </div>
-              <div className="question"> 
-              <input type="checkbox" />
-                <label htmlFor="2"> Have you had anaphylaxis to another vaccine or medication?</label>
-                <p> </p>
-              </div>
               <div className="question"> 
               <input type="checkbox" />
                 <label htmlFor="3"> Have you had COVID-19 before?</label>
                 <p> </p>
               </div>
               <div className="question"> 
-              <input type="checkbox" />
+              <label htmlFor="1"> Have you had an allergic reaction to a previous dose of a COVID-19 vaccine?</label>
+                <select id="allergic" name="allergic" placeholder="Select...">
+                  <option value="select">Select...</option>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
+                
+                <p> </p>
+              </div>
+              <div className="question"> 
+                <label htmlFor="2"> Have you had anaphylaxis to another vaccine or medication?</label>
+                <select id="allergic" name="allergic" placeholder="Select...">
+                  <option value="select">Select...</option>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
+                {/* <input type="text"></input> */}
+                <p> </p>
+              </div>
+              
+              <div className="question"> 
                 <label htmlFor="4"> Do you have a bleeding disorder?</label>
+                <select id="allergic" name="allergic" placeholder="Select...">
+                  <option value="select">Select...</option>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                 <p> </p>
               </div>
               <div className="question"> 
-              <input type="checkbox" />
                 <label htmlFor="5"> Do you take any medicine to thin your blood (an anticoagulant therapy)?</label>
+                <select id="allergic" name="allergic" placeholder="Select...">
+                  <option value="select">Select...</option>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                 <p> </p>
               </div>
               <div className="question"> 
-              <input type="checkbox" />
                 <label htmlFor="6"> Do you have a weakened immune system (immunocompromised)?</label>
+                <select id="allergic" name="allergic" placeholder="Select...">
+                  <option value="select">Select...</option>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                 <p> </p>
               </div>
               <div className="question"> 
-              <input type="checkbox" />
                 <label htmlFor="7"> Are you pregnant? *</label>
+                <select id="allergic" name="allergic" placeholder="Select...">
+                  <option value="select">Select...</option>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                 <p> </p>
               </div>
-              <div className="question"> 
-              <input type="checkbox" />
-                <label htmlFor="8"> Have you been sick with a cough, sore throat, fever or are feeling sick in another way? </label>
+              <div className="question fluSym"> 
+              <div className = "fluSym">
+                <label htmlFor="8"> Have you been sick with any of the following flu like symptoms? </label>
+                <div>
+                  <input type="checkbox" id="cough" name="cough"/>
+                  <label htmlFor="cough">Cough</label>
+                </div>
+                <div>
+                  <input type="checkbox" id="soreThroat" name="soreThroat"/>
+                  <label htmlFor="soreThroat">Sore Throat</label>
+                </div>
+                <div>
+                  <input type="checkbox" id="fever" name="fever"/>
+                  <label htmlFor="fever">Fever</label>
+                </div>
+                <div>
+                  <input type="checkbox" id="otherFluSym" name="otherFluSym"/>
+                  <label htmlFor="otherFluSym">Other Flu like symptoms</label>
+                </div>
+                <div className="clearfix"></div>
                 <p> </p>
+                </div>
               </div>
               <div className="question"> 
-              <input type="checkbox" />
-                <label htmlFor="9"> Have you had a COVID-19 vaccination before?</label>
-                <p> </p>
-              </div>
-              <div className="question"> 
-              <input type="checkbox" />
                 <label htmlFor="10">Have you received any other vaccination in the last 7 days?</label>
+                <select id="allergic" name="allergic" placeholder="Select...">
+                  <option value="select">Select...</option>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                 <p> </p>
               </div>
               <div className="question"> 
-              <input type="checkbox" />
                 <label htmlFor="11"> Have you ever been diagnosed with capillary leak syndrome?</label>
+                <select id="allergic" name="allergic" placeholder="Select...">
+                  <option value="select">Select...</option>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                 <p> </p>
               </div>
-              <div className="question"> 
-              <input type="checkbox" />
+              <div className="question">
                 <label htmlFor="13"> Have you ever had cerebral venous sinus thrombosis? *</label>
+                <select id="allergic" name="allergic" placeholder="Select...">
+                  <option value="select">Select...</option>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                 <p> </p>
               </div>
-              <div className="question"> 
-              <input type="checkbox" />
+              <div className="question">
                 <label htmlFor="14">Have you ever had heparin-induced thrombocytopenia? *</label>
+                <select id="allergic" name="allergic" placeholder="Select...">
+                  <option value="select">Select...</option>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                 <p> </p>
               </div>
               <div className="question"> 
-              <input type="checkbox" />
                 <label htmlFor="15"> Have you ever had blood clots in the abdominal veins (splanchnic veins)? *</label>
+                <select id="allergic" name="allergic" placeholder="Select...">
+                  <option value="select">Select...</option>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                 <p> </p>
               </div>
               <div className="question"> 
-              <input type="checkbox" />
                 <label htmlFor="16"> Have you ever had antiphospholipid syndrome associated with blood clots? *</label>
+                <select id="allergic" name="allergic" placeholder="Select...">
+                  <option value="select">Select...</option>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                 <p> </p>
               </div>
-              <div className="question"> 
-              <input type="checkbox" />
+              <div className="question">
                 <label htmlFor="17"> Are you under 60 years of age? *</label>
+                <select id="allergic" name="allergic" placeholder="Select...">
+                  <option value="select">Select...</option>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                 <p> </p>
               </div>
-              <div className="question"> 
+              <div className="question otherHealth"> 
                 <p>Other health conditions</p>
-                <input {...register("Health Conditions")} />
+                <textarea type="text" rows="10" cols="100" {...register("Health Conditions")} />
               </div> 
               <div className="submit">
                 <input type="submit" value="Submit"/>
@@ -142,7 +215,8 @@ const Bookings = () => {
           <p> </p>
 
         </div>
-
+      //</div>
+      
     );
        
 
