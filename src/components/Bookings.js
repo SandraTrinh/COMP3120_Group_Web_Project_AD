4e584a364a0d4d9d6ab43a0de9cf7a81622419a9
SandraTrinh <sandra.trinh@students.mq.edu.au>
2021-10-26 00:42:05 +1100

    made home page mobile friendly

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -14,10 +14,9 @@ import { useForm } from "react-hook-form";
 
 
 const Bookings = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
-  
-    console.log(watch("example")); // watch input value by passing the name of it
+
   
   
     const [myDate, setMyDate] = useState(new Date());
@@ -40,104 +39,109 @@ const Bookings = () => {
       });
       
     return (
-        <div >
-      <DatePicker selected={myDate} onChange={(date) => setMyDate(date)} />
-      <Select  styles={customStyles}  options={choices}      
-      />
-     
-     <form onSubmit={handleSubmit(onSubmit)}>
-         
-        <div> 
-          <input type="checkbox" />
-          <label htmlFor="1"> Have you had an allergic reaction to a previous dose of a COVID-19 vaccine?</label>
-          <p> </p>
-        </div>
-        <div> 
-        <input type="checkbox" />
-          <label htmlFor="2"> Have you had anaphylaxis to another vaccine or medication?</label>
-          <p> </p>
-        </div>
-        <div> 
-        <input type="checkbox" />
-          <label htmlFor="3"> Have you had COVID-19 before?</label>
-          <p> </p>
-        </div>
-        <div> 
-        <input type="checkbox" />
-          <label htmlFor="4"> Do you have a bleeding disorder?</label>
-          <p> </p>
-        </div>
-        <div> 
-        <input type="checkbox" />
-          <label htmlFor="5"> Do you take any medicine to thin your blood (an anticoagulant therapy)?</label>
-          <p> </p>
-        </div>
-        <div> 
-        <input type="checkbox" />
-          <label htmlFor="6"> Do you have a weakened immune system (immunocompromised)?</label>
-          <p> </p>
-        </div>
-        <div> 
-        <input type="checkbox" />
-          <label htmlFor="7"> Are you pregnant? *</label>
-          <p> </p>
-        </div>
-        <div> 
-        <input type="checkbox" />
-          <label htmlFor="8"> Have you been sick with a cough, sore throat, fever or are feeling sick in another way? </label>
-          <p> </p>
-        </div>
-        <div> 
-        <input type="checkbox" />
-          <label htmlFor="9"> Have you had a COVID-19 vaccination before?</label>
-          <p> </p>
-        </div>
-        <div> 
-        <input type="checkbox" />
-          <label htmlFor="10">Have you received any other vaccination in the last 7 days?</label>
-          <p> </p>
-        </div>
-        <div> 
-        <input type="checkbox" />
-          <label htmlFor="11"> Have you ever been diagnosed with capillary leak syndrome?</label>
-          <p> </p>
-        </div>
-        <div> 
-        <input type="checkbox" />
-          <label htmlFor="13"> Have you ever had cerebral venous sinus thrombosis? *</label>
-          <p> </p>
-        </div>
-        <div> 
-        <input type="checkbox" />
-          <label htmlFor="14">Have you ever had heparin-induced thrombocytopenia? *</label>
-          <p> </p>
-        </div>
-        <div> 
-        <input type="checkbox" />
-          <label htmlFor="15"> Have you ever had blood clots in the abdominal veins (splanchnic veins)? *</label>
-          <p> </p>
-        </div>
-        <div> 
-        <input type="checkbox" />
-          <label htmlFor="16"> Have you ever had antiphospholipid syndrome associated with blood clots? *</label>
-          <p> </p>
-        </div>
-        <div> 
-        <input type="checkbox" />
-          <label htmlFor="17"> Are you under 60 years of age? *</label>
-          <p> </p>
-        </div>
-        <div> 
-        <p>Other health conditions</p>
-          <input {...register("Health Conditions")} />
-          <input type="submit" value="Submit"/>
-        </div> 
+        <div className="bookingform" >
+          <div className="question">
+            <DatePicker selected={myDate} onChange={(date) => setMyDate(date)} />
+          </div>
+         <div className="question">
+            <Select  styles={customStyles}  options={choices}/>
+         </div>
+                  
+          <form onSubmit={handleSubmit(onSubmit)}>
+              
+              <div className="question"> 
+                <input type="checkbox" />
+                <label htmlFor="1"> Have you had an allergic reaction to a previous dose of a COVID-19 vaccine?</label>
+                <p> </p>
+              </div>
+              <div className="question"> 
+              <input type="checkbox" />
+                <label htmlFor="2"> Have you had anaphylaxis to another vaccine or medication?</label>
+                <p> </p>
+              </div>
+              <div className="question"> 
+              <input type="checkbox" />
+                <label htmlFor="3"> Have you had COVID-19 before?</label>
+                <p> </p>
+              </div>
+              <div className="question"> 
+              <input type="checkbox" />
+                <label htmlFor="4"> Do you have a bleeding disorder?</label>
+                <p> </p>
+              </div>
+              <div className="question"> 
+              <input type="checkbox" />
+                <label htmlFor="5"> Do you take any medicine to thin your blood (an anticoagulant therapy)?</label>
+                <p> </p>
+              </div>
+              <div className="question"> 
+              <input type="checkbox" />
+                <label htmlFor="6"> Do you have a weakened immune system (immunocompromised)?</label>
+                <p> </p>
+              </div>
+              <div className="question"> 
+              <input type="checkbox" />
+                <label htmlFor="7"> Are you pregnant? *</label>
+                <p> </p>
+              </div>
+              <div className="question"> 
+              <input type="checkbox" />
+                <label htmlFor="8"> Have you been sick with a cough, sore throat, fever or are feeling sick in another way? </label>
+                <p> </p>
+              </div>
+              <div className="question"> 
+              <input type="checkbox" />
+                <label htmlFor="9"> Have you had a COVID-19 vaccination before?</label>
+                <p> </p>
+              </div>
+              <div className="question"> 
+              <input type="checkbox" />
+                <label htmlFor="10">Have you received any other vaccination in the last 7 days?</label>
+                <p> </p>
+              </div>
+              <div className="question"> 
+              <input type="checkbox" />
+                <label htmlFor="11"> Have you ever been diagnosed with capillary leak syndrome?</label>
+                <p> </p>
+              </div>
+              <div className="question"> 
+              <input type="checkbox" />
+                <label htmlFor="13"> Have you ever had cerebral venous sinus thrombosis? *</label>
+                <p> </p>
+              </div>
+              <div className="question"> 
+              <input type="checkbox" />
+                <label htmlFor="14">Have you ever had heparin-induced thrombocytopenia? *</label>
+                <p> </p>
+              </div>
+              <div className="question"> 
+              <input type="checkbox" />
+                <label htmlFor="15"> Have you ever had blood clots in the abdominal veins (splanchnic veins)? *</label>
+                <p> </p>
+              </div>
+              <div className="question"> 
+              <input type="checkbox" />
+                <label htmlFor="16"> Have you ever had antiphospholipid syndrome associated with blood clots? *</label>
+                <p> </p>
+              </div>
+              <div className="question"> 
+              <input type="checkbox" />
+                <label htmlFor="17"> Are you under 60 years of age? *</label>
+                <p> </p>
+              </div>
+              <div className="question"> 
+                <p>Other health conditions</p>
+                <input {...register("Health Conditions")} />
+              </div> 
+              <div className="submit">
+                <input type="submit" value="Submit"/>
+              </div>
 
-      </form>
-      <p> </p>
-      <p> </p>
+          </form>
+          <p> </p>
+          <p> </p>
 
-      </div>
+        </div>
 
     );
        
